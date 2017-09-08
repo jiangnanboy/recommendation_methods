7d9826f659ae1ba38fa839269d631ad10b22483e
ShiYan <2229029156@qq.com>
2017-09-08 18:42:12 +0800

    测试

--- main_test/test.py
+++ main_test/test.py
@@ -3,18 +3,19 @@
 __author__="yan.shi"
 
 import numpy as np
-
-from com.sy.reco.util.dataprocess import DataProcess
-from com.sy.reco.util.readrating import ReadRating
-from com.sy.reco.recommendation.userbased import UserBased
-from com.sy.reco.recommendation.itembased import ItemBased
-from com.sy.reco.recommendation.slopeone import SlopeOne
-from com.sy.reco.recommendation.matrix_factorization.lfm import LFM
-from com.sy.reco.recommendation.matrix_factorization.biaslfm import BiasLFM
-from com.sy.reco.recommendation.matrix_factorization.svdpp import SVDPP
+from com.sy.reco.recommendation.cf.itembased import ItemBased
+from com.sy.reco.recommendation.cf.userbased import UserBased
+from com.sy.reco.recommendation.cf.slopeone import SlopeOne
 from com.sy.reco.recommendation.matrix_factorization.als import ALS
 from com.sy.reco.recommendation.matrix_factorization.als_wr import ALSWR
+from com.sy.reco.recommendation.matrix_factorization.biaslfm import BiasLFM
+from com.sy.reco.recommendation.matrix_factorization.lfm import LFM
+from com.sy.reco.recommendation.matrix_factorization.nmf import NMF
+from com.sy.reco.recommendation.matrix_factorization.svdpp import SVDPP
+from com.sy.reco.recommendation.content.contentbased import ContentBased
 from com.sy.reco.similarity.cosine import Cosine
+from com.sy.reco.util.dataprocess import DataProcess
+from com.sy.reco.util.readrating import ReadRating
 
 class Test():
     #数据预处理
@@ -24,7 +25,7 @@ class Test():
     def readRatingMatrix(self,ratingMatrixPath):
         read=ReadRating()
         ratingMatrix=read.readRatingData(ratingMatrixPath)
-        return ratingMatrix.values[:,1:]#去除第一列是用户的标号，返回的是numpy中的ndarray类型
+        return ratingMatrix.values[:,1:]#去除第一列是用户的标号，返回的是numpy中的ndarray类型,返回评分数据
     #基于user的预测
     def userBased(self,ratingMatrix,user_vec):
         userBased=UserBased(ratingMatrix,user_vec)
@@ -71,11 +72,20 @@ class Test():
         als_wr=ALSWR(ratingMatrix,F,λ,α)
         predictRating=als_wr.iteration_train(max_iter)
         print(predictRating)
+    #NMF
+    def nmf(self,ratingMatrix,F,λ,α,max_iter):
+        nmf_rec=NMF(ratingMatrix,F,λ,α)
+        predictRating=nmf_rec.iteration_train(max_iter)
+        print(predictRating)
+    #contentbased
+    def contentRec(self,itemsNamesList,itemsFeatures,user_vec):
+        content_rec=ContentBased(itemsNamesList,itemsFeatures,user_vec)
+        content_rec.getRecItems(10)
 
 if __name__=='__main__':
     test=Test()
     ratingMatrix = test.readRatingMatrix('G:\\python workspace\\recommendation_methods\\data\\ratingmatrix.csv')
-    recType=8
+    recType=10
     if recType==1:#userBased
         test.userBased(ratingMatrix,ratingMatrix[0])#第一个用户测试
     elif recType==2:#itemBased
@@ -96,11 +106,28 @@ if __name__=='__main__':
         test.svdpp(ratingMatrix,5,0.001,0.01,1000)#原始评分矩阵，隐类个数，学习速率，正则化参数，迭代次数
     elif recType==7:#ALS
         # ratingMatrix = ratingMatrix[0:10, 0:20]
-        ratingMatrix = np.array([[1, 3, 5, 2, 4], [3, 5, 2, 3, 1], [5, 5, 4, 2, 1], [4, 5, 2, 1, 3], [4, 4, 2, 3, 5]])
-        test.als(ratingMatrix,2,0.01,500)#原始评分矩阵，隐类个数，正则化参数，迭代次数
+        #ratingMatrix = np.array([[1, 3, 5, 2, 4], [3, 5, 2, 3, 1], [5, 5, 4, 2, 1], [4, 5, 2, 1, 3], [4, 4, 2, 3, 5]])
+        ratingMatrix = np.array([[1, 3, 0, 2, 0], [3, 0, 2, 0, 1], [5, 5, 4, 0, 1], [0, 5, 2, 0, 3], [4, 0, 2, 3, 5]])
+        test.als(ratingMatrix,2,0.01,1000)#原始评分矩阵，隐类个数，正则化参数，迭代次数
     elif recType==8:#ALSWR
         # ratingMatrix = ratingMatrix[0:10, 0:20]
         ratingMatrix = np.array([[1, 3, 0, 2, 0], [3, 0, 2,0, 1], [5, 5, 4, 0, 1], [0, 5, 2, 0, 3], [4, 0, 2, 3, 5]])
         test.alsWR(ratingMatrix,2,0.01,0.5,500)#原始评分矩阵，隐类个数，正则化参数，置信因子，迭代次数
+    elif recType==9:#NMF
+        ratingMatrix = np.array([[1, 3, 0, 2, 0], [3, 0, 2, 0, 1], [5, 5, 4, 0, 1], [0, 5, 2, 0, 3], [4, 0, 2, 3, 5]])
+        test.nmf(ratingMatrix, 2, 0.01, 0, 1000)  # 原始评分矩阵，隐类个数，正则化参数，正则化参数选择因子，迭代次数
+    elif recType==10:#contentbased
+        #评分矩阵
+        ratingMatrixPath='G:\\python workspace\\recommendation_methods\\data\\ratingmatrix.csv'
+        read = ReadRating()
+        rating_Matrix = read.readRatingData(ratingMatrixPath)
+        #item内容特征矩阵
+        itemContentMatrixPath='G:\\python workspace\\recommendation_methods\\data\\moviesContent.csv'
+        itemContentMatrix=read.readItemContent(itemContentMatrixPath)
+        #item名
+        itemsNamesList=list(rating_Matrix.columns[1:])#ratingMatrix第0列是用户列，从1列开始是item列
+        #所有item的特征矩阵
+        itemsFeatures=itemContentMatrix.values[:,1:]#itemContentMatrix第0列是item列，其余是item的特征分布
+        test.contentRec(itemsNamesList,itemsFeatures,ratingMatrix[0])
 
 
